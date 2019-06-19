a9899eb2fd41e98f091e1cbb0f0a70ae72959e00
Ben Attali <ben.attali8@gmail.com>
2019-06-19 16:35:54 +0300

    Mister cocktail -- refactored backend

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,10 +6,8 @@ class DosesController < ApplicationController
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    @dose.ingredient = @ingredient
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -27,6 +25,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
