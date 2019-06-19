91fdcdc2375567045651e00e904e292aadd00598
Ben Attali <ben.attali8@gmail.com>
2019-06-19 19:05:05 +0300

    Mister cocktail -- need to implement frontend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,8 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import 'bootsnap';
+import 'bootstrap';
+import 'select2/dist/css/select2.css';
+import { initSelect2 } from '../components/init_select2';
+
+initSelect2();
