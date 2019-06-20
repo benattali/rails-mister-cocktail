b942da95b289db0558db786a1007547610583e80
Ben Attali <ben.attali8@gmail.com>
2019-06-20 18:28:56 +0300

    Mister cocktail -- still need more frontend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,5 +18,7 @@
 import 'bootstrap';
 import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../components/init_select2';
+import { tooltip } from '../components/tooltip';
 
 initSelect2();
+tooltip();
